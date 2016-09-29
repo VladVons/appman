26355022dff2fcbf3ca85b792f3c202a1edf44a1
linux <vladvons@gmail.com>
2016-09-29 22:41:02 +0300

    just commit

--- install.sh
+++ install.sh
@@ -62,29 +62,32 @@ Clean()
 
 GitCreate()
 {
+  # create new project on disk
   git init
+
+  # sign with eMail
   git config --global user.email "vladvons@gmail.com"
 
   # no password 
   git config --global credential.helper 'cache --timeout=36000'
-  #git config --global credential.helper wincred
 
-  git add -A
-  git commit -m "first commit"
+  # remote git server location
   git remote add origin https://github.com/VladVons/appman.git
-  git push -u origin master
 
-  #web admin
+}
+
+GitClone()
+{
+  # restore clone copy fromserver to disk 
+  git clone https://github.com/VladVons/appman.git
+
+  #web admin access here
   #https://github.com/VladVons/appman
 }
 
-GitDownload()
-{
-  git clone https://github.com/VladVons/appman.git
-}
 
-
-GitUpload()
+GitSyncToServ()
+# sync only changes from disk to server 
 {
   git status
 
@@ -93,16 +96,24 @@ GitUpload()
   #git mv README.md README
   #git log
 
-  #git add -u -v
+  git add -u -v
   git commit -a -m "just commit"
   git push -u origin master
 }
 
-GitUpdate()
+GitFromServ()
+# sync changes from server to disk
+{
+  git pull
+}
+
+GitToServ()
+# sync changes from disk to serv
 {
   Clean
+  # add all new files
   git add -A -v
-  GitUpload
+  GitSyncToServ
 }
 
 
@@ -112,7 +123,8 @@ clear
 case $1 in
     Clean)          "$1"        "$2" "$3" ;;
     GitCreate)      "$1"        "$2" "$3" ;;
-    GitUpdate|u)    GitUpdate   "$2" "$3" ;;
+    GitToServ|t)    GitToServ   "$2" "$3" ;;
+    GitFromServ|f)  GitFromServ "$2" "$3" ;;
     Install)        "$1"        "$2" "$3" ;;
     ServiceRun)     "$1"        "$2" "$3" ;;
 esac
