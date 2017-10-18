be3692356a01b1b83074dd38abe6a86789cefb7b
linux <vladvons@gmail.com>
2017-10-18 09:40:44 +0300

    just commit

--- src/plugin/greenery/greenery.py
+++ src/plugin/greenery/greenery.py
@@ -7,23 +7,16 @@ import multiprocessing
 from inc.Common import *
 from Section import *
 from session import *
-
-
-#---
-class TLoad():
-    def Test(self):
-        print('Name')
+from pio import *
 
 
 #---
 class Gpio(TSectionVarExec):
     def __init__(self, aParent, aName):
         TSectionVarExec.__init__(self, aParent, aName)
+        self.Parent  = aParent
         self.Running = False
-        #self.Obj = {}
-
-    def Test(self):
-        print('Name', self.Name, 'Startup', self.GetVar('Startup'), 'GetKeys', self.GetKeys(), 'Gpio', self.GetValue('WaterPump'), self.Data)
+        self.Obj = {}
 
     def __RunThread(self, aName):
         GpioSession = TGpioSession(self, aName)
@@ -35,6 +28,12 @@ class Gpio(TSectionVarExec):
         process.start()
         time.sleep(0.5)
 
+    def Parse(self, aValue):
+        ClassName = self.GetField(aValue +'/Class')
+        if (ClassName):
+            TCl = globals()[ClassName]
+            self.Obj[aValue] = TCl()
+
     def Run(self):
         if (not self.Running):
             self.Running = True
@@ -43,10 +42,13 @@ class Gpio(TSectionVarExec):
             for Item in Str.split(cObjDelim):
                 if (Item in self.GetKeys()):
                     #self.__CreateThread(self.__RunThread, [Item])
-                    print('Start %s' % Item)
-                else:
-                    print('Gpio has no %s' % Item)
+                    self.Parse(Item )
+                #else:
+                #    print('Gpio has no %s' % Item)
 
-            while True:
-                time.sleep(10)
+            #while True:
+            #    time.sleep(10)
 
+    def Test(self):
+        print('Name', self.Name, 'Startup', self.GetVar('Startup'), 'GetKeys', self.GetKeys(), 'Gpio', self.GetValue('WaterPump'),  self.GetField('WaterPump'))
+        pass
