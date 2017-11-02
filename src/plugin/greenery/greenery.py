1aad09c2657a2543d5f1b479110f861ca99b1eda
VladVons <vladvons@gmail.com>
2017-11-02 06:35:05 +0200

    MyCommit

--- src/plugin/greenery/greenery.py
+++ src/plugin/greenery/greenery.py
@@ -1,6 +1,8 @@
 # Created: 13.10.2017
 # Vladimir Vons, VladVons@gmail.com
 
+
+
 import time
 import multiprocessing
 #
@@ -15,13 +17,19 @@ class Gpio(TSectionVarExec):
         super().__init__(aParent, aName)
 
     def Test(self):
-        print('Name', self.Name, 'Startup', self.GetVar('Startup'), 'GetKeys', self.GetKeys(), 'Gpio', self.GetValue('WaterPump'),  self.GetField('WaterPump'))
+        print('Name:', self.Name, ', GetKeys:', self.GetKeys(), ', Field', self.GetField('Run/Loop'))
+        #print('Run:', self.GetVar('Run'), self.GetValue('WaterPump'))
 
-    def OnState(self, aObj):
-        print('OnState', aObj.Alias)
+    def CallBack_OnValue(self, aObj):
+        Tag = aObj.Tag
+        if (Tag):
+            Info = 'Tag {:15}, Alias {:25}, Value {:8}, State {:1}'.format(aObj.Tag, aObj.Alias, aObj.Value, aObj.Param.State)
+            print('OnValue', Info)
 
     def Run(self):
+        self.Test()
+
         Manager = TManager()
-        Manager.OnState = self.OnState
+        Manager.OnValue = self.CallBack_OnValue
         Manager.Load(self.Data)
         Manager.Run()
